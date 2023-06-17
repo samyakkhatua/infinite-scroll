c55cd61019eba7f2a0246b95696f016faf0f9830
samyakkhatua <samyakkhatua@duck.com>
2023-06-17 23:40:32 +0530

    Scroll events used

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const container = document.querySelector('.container')
-const URL = 'https://xsgames.co/randomusers/assets/avatars/female/'
+const URL = 'http://xsgames.co/randomusers/assets/avatars/female/'
 
 
 function getRandom() {
     return Math.floor(Math.random()*75)
 }
 
-function loadImages(numImages = 10){
+function loadImages(numImages = 20){
     let i=0;
     while(i<numImages){
         const img = document.createElement('img')
-        img.src = `${URL}${getRandom().jpg}`
+        img.src = `${URL}${getRandom()}.jpg`
         container.appendChild(img)
         i++
     }
 }
 
 loadImages()
+
+window.addEventListener('scroll', () => {
+    
+    if ((window.scrollY + window.innerHeight >= document.documentElement.scrollHeight)) {
+        // loadImages()
+        setTimeout(loadImages, 1000)
+    }
+})
